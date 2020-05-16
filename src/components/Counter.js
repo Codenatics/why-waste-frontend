76808a66e0b6f614bd3f394b08930e06e577222a
Alba Rodriguez <albarodriguezgalvez@gmail.com>
2020-05-16 22:00:42 +0100

    conditional counter message added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "../assets/Counter.css"
 
-function Counter(props){
-    return(
-        <p>There are {props.count} meals available:</p>
+function Counter(props) {
+
+    return (
+        <div className="container-counter">
+            {props.count === 0 ? <p>Sorry, we didn't find any meals available here</p>: <p>There are {props.count} meals available:</p>}
+        </div>
     )
 }
 export default Counter;
