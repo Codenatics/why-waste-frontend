e62814a8a6fb448ed5954ec04a9286fa2c3c8f65
Alba Rodriguez <albarodriguezgalvez@gmail.com>
2020-05-16 21:49:33 +0100

    postcode validation

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -6,16 +6,30 @@ import { Link, BrowserRouter } from "react-router-dom";
 function SearchBox(props) {
 
     const [search, setSearch] = useState("")
-
+    const validPostcode = /^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$/.test(search)
+    
+    // const[isValid, setIsValid] = useState(false)
+    
     const updatedSearch = (event) => {
         setSearch(event.target.value)
     }
 
+    // const validateMessage = (event) => {
+    //     setIsValid(true)
+    // }
+
     return (
-        <form className="search-box">
-            <input type="text" placeholder="Enter your postcode" value={search} onChange={updatedSearch}></input>
-            <Link to={`/SearchResults?postcode=${search}`}><button class="btn btn-search">Search</button></Link>
-        </form>
+        <React.Fragment>
+            <form className="search-box">
+                <label className="label-postcode">
+                <h5>Enter your postcode:</h5>
+                </label>
+                <input type="text" placeholder="e.g. M1 3HE" value={search} onChange={updatedSearch} aria-required="true"></input>
+                {!validPostcode? <button class="btn btn-search" disabled>Search</button>:
+                <Link to={`/SearchResults?postcode=${search}`}><button class="btn btn-search">Search</button></Link>}
+                {search.length > 5 && !validPostcode ? <h6 className="validation">We couldn't recognise that postcode – check and try again.</h6> : ""}
+            </form>
+        </React.Fragment>
     )
 }
 
--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,46 +1,49 @@
 import React from "react";
 import "../../assets/forms.css";
+import Toolbar from "../Toolbar"
 import { Link } from "react-router-dom";
 
 function Register() {
     return (
-        <form className="donate-form">
-            <header>
-                <h1>Register today!</h1>
-            </header>
+        <React.Fragment>
+            <Toolbar />
+            <form className="donate-form">
+                <header>
+                    <h1>Register today!</h1>
+                </header>
 
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="companyName">Company name</label>
-                    <input type="text" class="form-control" id="companyName" />
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="companyName">Company name</label>
+                        <input type="text" class="form-control" id="companyName" />
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="email">E-mail</label>
+                        <input type="text" class="form-control" id="e-mail" />
+                    </div>
                 </div>
-                <div class="form-group col-md-6">
-                    <label for="email">E-mail</label>
-                    <input type="text" class="form-control" id="e-mail" />
+                <div class="form-group">
+                    <label for="address">Address</label>
+                    <input type="text" class="form-control" id="address" />
                 </div>
-            </div>
-            <div class="form-group">
-                <label for="address">Address</label>
-                <input type="text" class="form-control" id="address"/>
-            </div>
-            <div class="form-group">
-                <label for="inputAddress2">Address 2</label>
-                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
-            </div>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="inputCity">City</label>
-                    <input type="text" class="form-control" id="inputCity" />
+                <div class="form-group">
+                    <label for="inputAddress2">Address 2</label>
+                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
                 </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="inputCity">City</label>
+                        <input type="text" class="form-control" id="inputCity" />
+                    </div>
 
-                <div class="form-group col-md-2">
-                    <label for="inputPostcode">Postcode</label>
-                    <input type="text" class="form-control" id="inputPostcode" />
+                    <div class="form-group col-md-2">
+                        <label for="inputPostcode">Postcode</label>
+                        <input type="text" class="form-control" id="inputPostcode" />
+                    </div>
                 </div>
-            </div>
-            <button type="submit" class="btn btn-primary">Save</button>
-            <Link to="/"><button type="submit" class="btn btn-secondary">Back</button></Link>
-        </form>
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+        </React.Fragment>
     )
 }
 
