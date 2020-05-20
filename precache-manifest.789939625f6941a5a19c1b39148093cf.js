3f3f847154658098b93efc4342759cba658426ef
Alba Rodriguez <albarodriguezgalvez@gmail.com>
2020-05-20 08:26:48 +0100

    Updates

---
+++ precache-manifest.789939625f6941a5a19c1b39148093cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4cbd08a8f1b0cae0b6f4a113efe118c",
+    "url": "/why-waste-frontend/index.html"
+  },
+  {
+    "revision": "78fe6b36a6db03cbea4c",
+    "url": "/why-waste-frontend/static/css/2.3665d427.chunk.css"
+  },
+  {
+    "revision": "0258db1c87231389bce4",
+    "url": "/why-waste-frontend/static/css/main.bd6b21ad.chunk.css"
+  },
+  {
+    "revision": "78fe6b36a6db03cbea4c",
+    "url": "/why-waste-frontend/static/js/2.f5d80889.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/why-waste-frontend/static/js/2.f5d80889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0258db1c87231389bce4",
+    "url": "/why-waste-frontend/static/js/main.38805867.chunk.js"
+  },
+  {
+    "revision": "6c367414bc74db307524",
+    "url": "/why-waste-frontend/static/js/runtime-main.613bf6ad.js"
+  }
+]);
